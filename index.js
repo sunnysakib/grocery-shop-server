e553b1aa6a334cb9cd9675f8ded550d911269bde
sunnysakib <sakibur.rahaman033@gmail.com>
2021-04-09 00:17:10 +0600

    fix

--- index.js
+++ index.js
@@ -69,6 +69,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
